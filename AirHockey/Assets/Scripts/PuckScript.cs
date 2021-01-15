850c3861433befa6c76edbf209a6ac74eb26ced4
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2021-01-15 16:18:40 +0900

    Air Hockey initial commit

---
+++ AirHockey/Assets/Scripts/PuckScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PuckScript : MonoBehaviour
+{
+    public ScoreScript ScoreScriptInstance;
+    public static bool WasGoal { get; private set; }
+
+    public AudioManager audioManager;
+
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        WasGoal = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!WasGoal)
+        {
+            if (other.tag == "AIGoal")
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.PlayerScore);
+                WasGoal = true;
+                audioManager.PlayGoal();
+                StartCoroutine(ResetPuck());
+            }
+            else if (other.tag == "PlayerGoal")
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.AIScore);
+                WasGoal = true;
+                audioManager.PlayGoal();
+                StartCoroutine(ResetPuck());
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioManager.PlayPuckCollision();
+    }
+
+    private IEnumerator ResetPuck()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        WasGoal = false;
+        rb.velocity = rb.position = new Vector2(0, -0.1f);
+    }
+}
---
+++ AirHockey/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+ 
+public class ScoreScript : MonoBehaviour
+{
+    public enum Score
+    {
+        AIScore, PlayerScore
+    }
+
+    public Text AIScoreText, PlayerScoreText;
+    private int aiScore, playerScore;
+
+    public void Increment(Score whichScore)
+    {
+        if (whichScore == Score.AIScore)
+            AIScoreText.text = (++aiScore).ToString();
+        else
+            PlayerScoreText.text = (++playerScore).ToString();
+    }
+}
