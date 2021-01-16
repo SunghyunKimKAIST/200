1ee61b3bc746b0a081b1d45071c9eb11ea6d85e0
SunghyunKimKAIST <harryzzong@kaist.ac.kr>
2021-01-16 23:16:52 +0900

    initial commit

---
+++ MadCamp/Assets/Scripts/Boss.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    public GameObject bulletPrefab;
+
+    float velocity;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(PatternSlide(8, 1, 2, 1));
+        Invoke("makePatternBullet", 7);
+    }
+
+    IEnumerator PatternSlide(int speed, int direction, float slideTime, float intervalTime)
+    {
+        while (true)
+        {
+            spriteRenderer.flipX = direction == 1;
+            velocity = speed * direction;
+            yield return new WaitForSeconds(slideTime / 2);
+            rigid.AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+            yield return new WaitForSeconds(slideTime / 2);
+            velocity = 0;
+            yield return new WaitForSeconds(intervalTime);
+            direction *= -1;
+        }
+    }
+
+    IEnumerator PatternBullet(float speed, float intervalTime, float destroyTime)
+    {
+        while (true)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+            rigid.velocity = new Vector2(speed, 0);
+            Destroy(bullet, destroyTime);
+            yield return new WaitForSeconds(intervalTime);
+        }
+    }
+
+    void makePatternBullet()
+    {
+        StopAllCoroutines();
+        velocity = 0;
+        StartCoroutine(PatternBullet(10, 1, 2));
+    }
+
+    void FixedUpdate()
+    {
+        // Move
+        rigid.velocity = new Vector2(velocity, rigid.velocity.y);
+    }
+}
---
+++ MadCamp/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerMovement player;
+
+    int hp;
+    public Image[] UIhp;
+
+    void Awake()
+    {
+        hp = 3;
+    }
+
+    public void HealthDown()
+    {
+        hp--;
+        UIhp[hp].color = new Color(1, 1, 1, 0.2f);
+
+        if(hp <= 0)
+        {
+            player.OnDie();
+        }
+    }
+}
